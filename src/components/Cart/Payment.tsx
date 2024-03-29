bcc09d896b8f1800a34f95d220e3dc18d6f6d591
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2024-03-29 23:54:02 +0530

    added toast

--- src/components/Cart/Payment.tsx
+++ src/components/Cart/Payment.tsx
@@ -1,15 +1,48 @@
-import { useCart } from "../../contexts/CartContext"
+import { useCart } from "../../contexts/CartContext";
+import { toast, Bounce, ToastContainer, } from 'react-toastify'; // Import Bounce from react-toastify
+import 'react-toastify/dist/ReactToastify.css';
 
 const Payment = () => {
+    const { totalPrice } = useCart();
 
-    const {totalPrice} = useCart()
-  
+    const handleToast = () => {
+        
+      if(totalPrice > 0) {
+        toast.success('Thanks for shopping ', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+      }
+      else{
+        toast.error('Please add items to the cart', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+      });
+      }
+        
+    }
+
     return (
-   <>
-        <span className="text-lg font-semibold">Total Amount : {totalPrice}</span>
-        <button className="bg-orange-500 text-white p-1 px-2 rounded-xl">Pay</button>
-    </>
-  )
+        <>
+            <span className="text-lg font-semibold">Total Amount : {totalPrice}</span>
+            <button className="bg-orange-500 text-white p-1 px-2 rounded-xl" onClick={handleToast}>Pay</button>
+            <ToastContainer />
+        </>
+    );
 }
 
-export default Payment
+export default Payment;
